584b6c488ea6dd139e64b8b93b8bc4b27ae5304e
JAAM4494 <arielarias7@estudiantec.cr>
2015-03-31 10:23:46 -0600

    SecondCommit

--- src/compiladoresfirstproject/Main.java
+++ src/compiladoresfirstproject/Main.java
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // Metodos para generar los .java de los analizadores
-        // los comentan despues de generarlos
+        // los comentan despues de generarlos (las 2 lineas de abajo)
         generarScanner();
         generarParser();
         //---------------------------------------------------
