9c9bbf43b72ec6818445e2d5ac400644cb638013
JAAM4494 <arielarias7@estudiantec.cr>
2015-03-31 13:36:44 -0600

    Se quitan errores .cup

--- src/compiladoresfirstproject/Main.java
+++ src/compiladoresfirstproject/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         // Metodos para generar los .java de los analizadores
         // los comentan despues de generarlos (las 2 lineas de abajo)
-        generarScanner();
+        //generarScanner();
         generarParser();
         //---------------------------------------------------
 
