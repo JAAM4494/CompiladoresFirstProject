82624ffee62df70dfdf06811cdb7083b0957f3c6
JAAM4494 <arielarias7@estudiantec.cr>
2015-04-20 23:12:41 -0600

    Cambios intermedio..

--- src/compiladoresfirstproject/Intermedio.java
+++ src/compiladoresfirstproject/Intermedio.java
@@ -14,70 +14,72 @@ import java.util.Vector;
  * @author DANIEL
  */
 public class Intermedio {
-       
+
     private FileWriter fichero = null;
     private PrintWriter pw = null;
-    
-    public Intermedio(){
 
-        try
-        {
+    private String TempString = " ";
+    private boolean oneLine = false;
+
+    public Intermedio() {
+
+        try {
             fichero = new FileWriter("tabla.html");
             pw = new PrintWriter(fichero);
             pw.print("");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-           try {
-           // Nuevamente aprovechamos el finally para 
-           // asegurarnos que se cierra el fichero.
-           if (null != fichero)
-              fichero.close();
-           } catch (Exception e2) {
-              e2.printStackTrace();
-           }
+            try {
+                // Nuevamente aprovechamos el finally para 
+                // asegurarnos que se cierra el fichero.
+                if (null != fichero) {
+                    fichero.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
         }
     }
-    
-    public void writeSymbol(String pSim){
-            FileWriter fichero = null;
-             PrintWriter pw = null;
-            try
-        {
-            fichero = new FileWriter("intermedio.int",true);
+
+    public void writeSymbol(String pSim) {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try {
+            fichero = new FileWriter("intermedio.int", true);
             pw = new PrintWriter(fichero);
             pw.println(pSim);
             //System.out.println("Sim: "+pSim);
- 
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-           try {
-           // Nuevamente aprovechamos el finally para 
-           // asegurarnos que se cierra el fichero.
-           if (null != fichero)
-              fichero.close();
-           } catch (Exception e2) {
-              e2.printStackTrace();
-           }
+            try {
+                // Nuevamente aprovechamos el finally para 
+                // asegurarnos que se cierra el fichero.
+                if (null != fichero) {
+                    fichero.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
-    }
-    
+
     public void createInterStack(Vector pVector, String pToken, String pLexema) {
-        
-        if(pToken.equals("ID")){
+
+        if (pToken.equals("ID")) {
             pVector.add(pLexema);
         } else {
-            if(!pToken.equals("NewLine") & !pToken.equals("CloseParenth") & !pToken.equals("OpParenth")) {
+            if (!pToken.equals("NewLine") & !pToken.equals("CloseParenth") & !pToken.equals("OpParenth")) {
                 pVector.add(pLexema.toUpperCase());
             }
         }
     }
-    
-    
+
     public void debugInterSack(Vector pVector) {
-        
-        if(AnalizadorMain.canGenerateCode){
+
+        if (AnalizadorMain.canGenerateCode) {
             for (Object pVector1 : pVector) {
                 writeSymbol(pVector1.toString());
             }
